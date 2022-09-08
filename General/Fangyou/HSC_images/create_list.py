b66a2f0d0b5038ffcca1ead6359190f7606c0d2b
Karsten1999 <jespert.karsten@gmail.com>
2022-09-08 20:58:31 +0200

    Created python file for making a list to submit a query to HSC

---
+++ General/Fangyou/HSC_images/create_list.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+data_name = '../../../Data/Fangyou_data/Cleaned/combined_non_filled_preprocessed.csv'
+
+# Only selecting columns we are interested in
+data = pd.read_csv(data_name)[["Source_Name", "RA", "DEC", 'Source']]
+
+# Only selecting Elais-N1
+data = data[data['Source'] == 'Elais-N1']
+data = data.drop(columns='Source')
+
+# Settings
+rerun = 'pdr3_dud'
+filt = 'HSC-I'
+sw = sh = '5asec'
+image_bool = 'true'
+mask_bool = 'true'
+variance_bool = 'true'
+
+# Filling in the settings
+data['rerun'] = rerun
+data['ra'] = data['RA']
+data['dec'] = data['DEC']
+data = data.drop(columns=['RA', 'DEC'])
+data['sw'] = sw
+data['sh'] = sh
+data['filter'] = filt
+data['image'] = image_bool
+data['mask'] = mask_bool
+data['variance'] = variance_bool
+data['name'] = data['Source_Name']
+data = data.drop(columns='Source_Name')
+
+data.to_csv('test.txt', index=False, sep=' ', )
